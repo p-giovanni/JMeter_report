e5940a6d68e32b161c708264c5f13a42463bd5e6
Giovanni Rizzardi <rizzardig@gmail.com>
2022-02-17 16:35:24 +0100

    Better path handling.

--- run-create-report.sh
+++ run-create-report.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-PROJECT_HOME="/Users/ERIZZAG5J/repos/JMeter_report"
-PROG_PATH="./src/report/CreateReport.py"
 source /Users/ERIZZAG5J/venv/bin/activate
+
+PROJECT_HOME=$(cd $(dirname "$BASH_SOURCE[0]")&& pwd)
+PROG_PATH="./src/report/CreateReport.py"
 
 export PYTHONPATH="${PROJECT_HOME}/src:${PYTHONPATH}"
 
-python ${PROG_PATH} --report $1 $2 $3 --chart_title "VIDEO_API_Playback_STAGE"
-
+python ${PROJECT_HOME}/${PROG_PATH} --report $1 $2 $3 --chart_title "VIDEO_API_Playback_STAGE"
+exit $?
 
